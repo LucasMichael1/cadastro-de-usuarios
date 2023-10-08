c461c1ddc8820727ecbbf9958efec3c55899de43
LucasMichael1 <lucasmldm03@gmail.com>
2023-10-08 00:06:59 -0300

    cadastrousuarios

---
+++ app_cad_usuarios/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Usuario(models.Model):
+    objects = None
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=200)
+    idade = models.IntegerField()
